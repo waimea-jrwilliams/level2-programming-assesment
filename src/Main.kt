3607e809a19f81f5d987792124aa7a1013abd134
waimea-jrwilliams <jrwilliams@waimea.school.nz>
2025-03-18 14:47:05 +1300

    Project setup

--- src/Main.kt
+++ src/Main.kt
@@ -2,11 +2,14 @@
  * =====================================================================
  * Programming Project for NCEA Level 2, Standard 91896
  * ---------------------------------------------------------------------
- * Project Name:   PROJECT NAME HERE
- * Project Author: PROJECT AUTHOR HERE
- * GitHub Repo:    GITHUB REPO URL HERE
+ * Project Name:   Old Gold
+ * Project Author: Jiivahn Williams
+ * GitHub Repo:    https://github.com/waimea-jrwilliams/level2-programming-assesment
  * ---------------------------------------------------------------------
- * Notes:
+ * Notes:Old gold is a game with two players,
+ * its played on a one-dimensional grid with coins,
+ * where the aim is to win by being the player who
+ * removes the gold coin.
  * PROJECT NOTES HERE
  * =====================================================================
  */
