897f25595f3b82d89bb946a1aa1e8ed2d4a90804
waimea-jrwilliams <jrwilliams@waimea.school.nz>
2025-03-31 10:59:14 +1300

    nearly finished

--- src/Main.kt
+++ src/Main.kt
@@ -41,13 +41,12 @@ fun main() {
     println("+--------------------------------------------------------------------------------+")
     println("INSTRUCTIONS")
     println("There is two players player 1 & player 2.")
-    println("player one roles the die to see what number they get")
+    println("player one rolls the die to see what number they get")
     println("The dice consists of numbers from one to six,")
     println("whatever number it lands on would add to your points. ")
-    println("It goes on until one player looses to the other person")
+    println("It goes on until one player loses to the other person")
     println("get the most points in five rounds to win the game!.")
     println("+--------------------------------------------------------------------------------+")
-
 
     // Ask for player names
     println("You are player 1.")
@@ -64,55 +63,40 @@ fun main() {
 
     if (userResponse == "yes") {
         println("Great! Let's start the game.")
-        // Start the game
+
+        // Initialize scores
+        var score1 = 0
+        var score2 = 0
+
+        // Play 5 rounds
+        for (round in 1..5) {
+            println("--- Round $round ---")
+
+            // Player 1's Turn
+            score1 += rollDice(userName1)
+
+            // Player 2's Turn
+            score2 += rollDice(userName2)
+        }
+
+        // Declare the winner
+        println("Final Scores:")
+        println("$userName1: $score1 points")
+        println("$userName2: $score2 points")
+
+        if (score1 > score2) {
+            println("Winner: $userName1!")
+        } else if (score2 > score1) {
+            println("Winner: $userName2!")
+        } else {
+            println("It's a tie!")
+        }
     } else {
         println("Maybe next time!")
-        return
     }
 
-    println()
-    println("Lets get started!")
-    println()
-    println("Player one it is your turn to roll")
-    println("Type 'X' to roll")
-}
-
-fun main() {
-    // Initialize scores
-    var score1 = 0
-    var score2 = 0
-    // Play 5 rounds
-    for (round in 1..5) {
-        println("\n--- Round $round ---")
-
-        // Player 1's Turn
-        score1 += rollDice(userName1)
-
-        // Player 2's Turn
-        score2 += rollDice(userName2)
-    }
-
-    // Declare the winner
-    println("\nFinal Scores:")
-    println("$userName1: $score1 points")
-    println("$userName2: $score2 points")
-
-    if (score1 > score2) {
-        println("Winner: $userName1!")
-    } else if (score2 > score1) {
-        println("Winner: $UserName2!")
-    } else {
-        println("It's a tie!")
-    }
-     else {
-    println("Maybe next time!")
-}
-
-
-// Create a list
+    // Create a list
     val dices = mutableListOf<String>()
-
-    println(dices)
 
     // Add some things
     dices.add("1")
@@ -122,8 +106,9 @@ fun main() {
     dices.add("5")
     dices.add("6")
 
-
+    println(dices)
 }
+
 /**
  * Function to get a string from a user
  */
@@ -137,3 +122,4 @@ fun getString(prompt: String): String {
     }
     return userInput
 }
+
